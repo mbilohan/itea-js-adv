4c09c70c217f3ce77b92f3b3e68eb5c2e63fd167
Mariia Kostornychenko <m.kostornychenko@gmail.com>
2018-09-20 14:49:12 +0300

    task6

--- task6/github/js/main.js
+++ task6/github/js/main.js
@@ -1,15 +1,31 @@
 (function() {
     var els = getElements();
 
-    els.elements.btn.addEventListener('click', findBtnClick);
+    var sections = els.sections;
+    var elements = els.elements;
+
+    elements.btn.addEventListener('click', findBtnClick);
     function findBtnClick() {
-        if(els.elements.searchInput.value) {
-            gitapi.send('GET', 'users/' + els.elements.searchInput.value, function(result) {
-                els.sections.user.style.display = 'block';
-                els.elements.userAvatar.src = result.avatar_url;
-                els.elements.userName.innerText = result.name;
+        if(elements.searchInput.value) {
+            gitapi.send('GET', 'users/' + elements.searchInput.value, function(result) {
+                sections.user.style.display = 'block';
+                elements.userAvatar.src = result.avatar_url;
+                elements.userName.innerText = result.name;
+                elements.userRepos.dataset.userLogin = result.login;
             });
         }
+    }
+
+    elements.userRepos.addEventListener('click', findRepos);
+    function findRepos(e) {
+        gitapi.send('GET', 'users/' + e.target.dataset.userLogin + '/repos', function(result) {
+            sections.repos.style.display = 'block';
+            for(var i = 0; i < result.length; i++) {
+                var item = document.createElement('p');
+                item.innerText = result[i].full_name
+                sections.repos.appendChild(item);
+            }
+        });
     }
 
     function getElements() {
@@ -17,12 +33,14 @@
             sections: {
                 search: document.getElementById('search'),
                 user: document.getElementById('user'),
+                repos: document.getElementById('repos'),
             },
             elements: {
                 searchInput: document.getElementById('searchtext'),
                 btn: document.getElementById('findbtn'),
                 userAvatar: document.getElementById('useravatar'),
-                userName: document.getElementById('username')
+                userName: document.getElementById('username'),
+                userRepos: document.getElementById('userrepos')
             }
         }
     }
